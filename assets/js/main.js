aef509d639accd068f15c0ad55ea8d1ad74ec49e
András Tarsoly <tarsolya@gmail.com>
2018-10-07 11:08:33 +0200

    Template update and cleanup before deployment

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,27 +19,6 @@ jQuery(function($) {
 
     }
 
-    _Blog.changeTitle = function() {
-        var currentTitle = document.title;
-        window.onblur = function() {
-            document.title = 'I miss you!（＞﹏＜）';
-        }
-        window.onfocus = function() {
-            document.title = currentTitle;
-        }
-    };
-
-    _Blog.toggleTheme = function() {
-        const currentTheme = window.localStorage && window.localStorage.getItem('theme')
-        const isDark = currentTheme === 'dark'
-        $('body').toggleClass('dark-theme', isDark)
-        $('.theme-switch').on('click', () => {
-            $('body').toggleClass('dark-theme')
-            window.localStorage &&
-                window.localStorage.setItem('theme', document.body.classList.contains('dark-theme') ? 'dark' : 'light', )
-        })
-    }
-
     _Blog.toggleMobileMenu = function() {
         $('.menu-toggle').on('click', () => {
             $('.menu-toggle').toggleClass('active')
@@ -49,8 +28,6 @@ jQuery(function($) {
 
     $(document).ready(function() {
         _Blog.prettify()
-        _Blog.changeTitle()
-        _Blog.toggleTheme()
         _Blog.toggleMobileMenu()
     });
 });
